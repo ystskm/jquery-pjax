8a7eafb37f33726762df4a2489d7153236cc2fe8
ystskm <brilliantpenguin@gmail.com>
2011-09-29 15:32:47 +0900

    first commit

--- README.md
+++ README.md
@@ -71,13 +71,20 @@ Three. Unobtrusive, showing a 'loading' spinner:
 ```
 
 ```js
-$('a').pjax('#main').live('click', function(){
-  $(this).showLoader()
-})
+$('a').pjax('#main',{
+  callback:{
+    forward:function(){
+      $(this).showLoader();
+    },
+    back:function(){
+      $(this).hideLoader();
+    }
+  }
+});
 ```
 
 
-## $(link).pjax( container, options )
+## $(link).pjax( selector, url, options )
 
 The `$(link).pjax()` function accepts a container, an options object,
 or both. The container MUST be a string selector - this is because we
@@ -86,7 +93,7 @@ cannot persist jQuery objects using the History API between page loads.
 The options are the same as jQuery's `$.ajax` options with the
 following additions:
 
-* `container`      - The String selector of the container to load the
+* `selector`       - The String selector of the container to load the
                      reponse body. Must be a String.
 * `clickedElement` - The element that was clicked to start the pjax call.
 * `push`           - Whether to pushState the URL. Default: true (of course)
@@ -101,126 +108,11 @@ following additions:
                      into the `container`. Useful if the server always returns
                      full HTML pages.
 
-
-## $.pjax( options )
-
-You can also just call `$.pjax` directly. It acts much like `$.ajax`, even
-returning the same thing and accepting the same options.
-
-The pjax-specific keys listed in the `$(link).pjax()` section work here
-as well.
-
-This pjax call:
-
-```js
-$.pjax({
-  url: '/authors',
-  container: '#main'
-})
-```
-
-Roughly translates into this ajax call:
-
-```js
-$.ajax({
-  url: '/authors',
-  dataType: 'html',
-  beforeSend: function(xhr){
-    xhr.setRequestHeader('X-PJAX', 'true')
-  },
-  success: function(data){
-    $('#main').html(data)
-    history.pushState(null, $(data).filter('title').text(), '/authors')
-  })
-})
-```
-
-
 ## pjax on the server side
 
-You'll want to give pjax requests a 'chrome-less' version of your page.
-That is, the page without any layout.
-
-As you can see in the "ajax call" example above, pjax sets a custom 'X-PJAX'
-header to 'true' when it makes an ajax request to make detecting it easy.
-
-In Rails, check for `request.headers['X-PJAX']`:
-
-```ruby
-def my_page
-  if request.headers['X-PJAX']
-    render :layout => false
-  end
-end
-```
-
-Django: <https://github.com/jacobian/django-pjax>
-
-Asp.Net MVC3: <http://biasecurities.com/blog/2011/using-pjax-with-asp-net-mvc3/>
-
-
-## page titles
-
-Your HTML should also include a `<title>` tag if you want page titles to work.
-
-When using a page fragment, pjax will check the fragment DOM element
-for a `title` or `data-title` attribute and use any value it finds.
-
-
-## events
-
-pjax will fire two events on the container you've asked it to load your
-reponse body into:
-
-* `pjax:start` - Fired when a pjax ajax request begins.
-* `pjax:end`   - Fired when a pjax ajax request ends.
-
-This allows you to, say, display a loading indicator upon pjaxing:
-
-```js
-$('a.pjax').pjax('#main')
-$('#main')
-  .bind('pjax:start', function() { $('#loading').show() })
-  .bind('pjax:end',   function() { $('#loading').hide() })
-```
-
-Because these events bubble, you can also set them on the body:
-
-```js
-$('a.pjax').pjax()
-$('body')
-  .bind('pjax:start', function() { $('#loading').show() })
-  .bind('pjax:end',   function() { $('#loading').hide() })
-```
-
-## browser support
-
-pjax only works with browsers that support the history.pushState API.
-
-For a table of supported browsers see: <http://caniuse.com/#search=pushstate>
-
-To check if pjax is supported, use the `$.support.pjax` boolean.
-
-When pjax is not supported, `$('a').pjax()` calls will do nothing (aka links
-work normally) and `$.pjax({url:url})` calls will redirect to the given URL.
-
-
-## install it
-
-```
-$ cd path/to/js
-$ wget https://github.com/defunkt/jquery-pjax/raw/master/jquery.pjax.js
-```
-
-Then, in your HTML:
-
-```html
-<script src="path/to/js/jquery.pjax.js"></script>
-```
-
-Replace `path/to/js` with the path to your JavaScript directory,
-e.g. `public/javascripts`.
-
+This jquery.pjax.js can work without server setting.
+And if you want some branch on server, check the request header
+`request.headers['X-PJAX']`
 
 ## minimize it
 
@@ -228,6 +120,6 @@ e.g. `public/javascripts`.
 curl \
   -d output_info=compiled_code \
   -d compilation_level=SIMPLE_OPTIMIZATIONS \
-  -d code_url=https://github.com/defunkt/jquery-pjax/raw/master/jquery.pjax.js \
+  -d code_url=https://github.com/ystskm/jquery-pjax/raw/master/jquery.pjax.js \
   http://closure-compiler.appspot.com/compile
 ```
--- jquery.pjax.js
+++ jquery.pjax.js
@@ -1,40 +1,146 @@
-/* 
- * jquery.pjax.js
- * copyright ystskm
- * https://github.com/ystskm/jquery-pjax
- */
+// Copyright (c) ystskm
+// https://github.com/ystskm/jquery-pjax/LICENSE
 (function($) {
-  $.fn.pjax = function(selector, options) {
-    options = $.isPlainObject(selector) ? selector: {
-      selector: selector
-    };
 
-    var params = $.extend({
+  var initialURL = location.href;
+  var currentURL = null;
+  var pjaxMap = {};
+
+  $.fn.pjax = function(selector, url, options) {
+
+    // SPECIFICATION
+    // $('div').pjax('#main1','/test1').pjax('#main2','/test2') ...
+    
+    // !! LIMITATION !!
+    // CANNOT USE SAME URL IN HISTORY
+    // $('div').pjax('#main1','/test1').pjax('#main2','/test1') ...
+    // ...may occurs unexpected action.
+
+    if(!pjaxMap[this.selector])
+      pjaxMap[this.selector] = {
+        _self: this,
+        _selector: this.selector,
+        _histArr: [],
+        _histMap: {},
+        _params: {}
+      };
+
+    if($.isPlainObject(selector))
+      options = selector;
+    else if($.isPlainObject(url)) {
+      options = url;
+      options.selector = selector;
+    } else if($.isPlainObject(options)) {
+      options.selector = selector;
+      options.url = url;
+    } else
+      options = {
+        selector: selector,
+        url: url
+      };
+
+    var params = $.extend(true, pjax.defaults, {
       url: this.attr('href'),
       selector: this.attr('data-pjax'),
+      onchange: {
+        forward: null,
+        back: null
+      },
+      callback: {
+        forward: $.noop,
+        back: $.noop
+      }
     }, options);
     
+    // not overwritable parameters
+    params.trigger = this.selector;
+
     if($.isFunction(params.url))
       params.url = params.url();
+    if(!/^\//.test(params.url))
+      params.url = location.pathname.replace(/\/[^\/]*$/,'/') + params.url;
 
-    return this.click(function(event) {
-      console.log('clicked.');
-      $.pjax(params);
-    });
+    if(!params.selector)
+      throw new Error(
+          'pjax must define attr("data-pjax") or options.selector .');
+
+    if(!params.url)
+      throw new Error('pjax must define attr("href") or options.url .');
+
+    if(!$.isFunction(params.onchange.forward))
+      params.onchange.forward = function(target, data) {
+        target.html(data);
+      };
+
+    if(!$.isFunction(params.onchange.back))
+      params.onchange.back = function(target, data) {
+        target.html(data);
+      };
+
+    // save params
+    var map = pjaxMap[this.selector];
+    if(!map._histMap[params.url])
+      map._histArr.push(params.url);
+    map._histMap[params.url] = map._histArr.length - 1;
+    map._params[params.url] = params;
+
+    // attach push handler
+    this.one('click', clkFn(map, params.url));
+
+    // binding popstate eventHandler again
+    $(window).one('popstate', popFn(map));
+
+    return this;
   };
-
+  function clkFn(map, url) {
+    return function(event) {
+      $.pjax(map._params[url]);
+    };
+  }
+  function popFn(map) {
+    return function(event) {
+      // Ignore initial pop state that some browsers fire on page load
+      if('state' in window.history && location.href == initialURL)
+        return;
+      // pop state action
+      var state = event.state;
+      if(state && state.pjax && state.trigger == map._selector) {
+        if($(state.pjax).length)
+          $.pjax({
+            trigger: state.trigger,
+            url: state.url || location.pathname,
+            selector: state.pjax,
+            push: false
+          });
+        else
+          window.location = location.href;
+      }
+    };
+  };
   // Returns whatever $.ajax returns.
   var pjax = $.pjax = function(options) {
+    var map = pjaxMap[options.trigger];
+    var histIdx = map._histMap[currentURL] != null ? map._histMap[currentURL]: -1;
+    var nextIdx = map._histMap[options.url] || 0;
+    var action = histIdx < nextIdx ? 'forward': 'back';
+    if(!options.success)
+      options = $
+          .extend(
+              {},
+              action == 'forward' ? map._params[options.url]: map._params[currentURL],
+              options);
 
     options.success = function(data) {
+      var target = $(options.selector);
 
       // Make it happen.
-      this.html(data);
+      // "this" is XHR Object
+      options.onchange[action].call(this, target, data);
 
       // If there's a <title> tag in the response, use it as
       // the page's title.
-      var oldTitle = document.title, title = $.trim(this.find('title').remove()
-          .text());
+      var oldTitle = document.title;
+      var title = $.trim(target.find('title').remove().text());
       if(title != null)
         document.title = title;
 
@@ -42,23 +148,25 @@
       if(!title && options.fragment)
         title = $fragment.attr('title') || $fragment.data('title');
 
+      // Make push state
       var state = {
+        trigger: options.trigger,
         pjax: options.selector,
-        fragment: options.fragment,
-        timeout: options.timeout
+        url: options.url
       };
 
       // If there are extra params, save the complete URL in the state object
-      var query = $.param(options.data);
-      if(query != "_pjax=true")
-        state.url = options.url + (/\?/.test(options.url) ? "&": "?") + query;
-
+      if(options.data != null) {
+        var query = $.param(options.data);
+        if(query != "_pjax=true")
+          state.url = options.url + (/\?/.test(options.url) ? "&": "?") + query;
+      }
       if(options.replace) {
         window.history.replaceState(state, document.title, options.url);
-      } else if(options.push) {
+      } else if(options.push === true) {
         // this extra replaceState before first push ensures good back
         // button behavior
-        if(!$.pjax.active) {
+        if($.pjax.active !== true) {
           window.history.replaceState($.extend({}, state, {
             url: null
           }), oldTitle);
@@ -66,19 +174,27 @@
         }
         window.history.pushState(state, document.title, options.url);
       }
-
       // Google Analytics support
       if((options.replace || options.push) && window._gaq)
         _gaq.push(['_trackPageview']);
-
       // If the URL has a hash in it, make sure the browser
       // knows to navigate to the hash.
       var hash = window.location.hash.toString();
       if(hash !== '')
         window.location.href = hash;
 
-      // Invoke their success handler if they gave us one.
-      success.apply(this, arguments);
+      // binding click event if first
+      if(location.href == initialURL)
+        map._self.one('click', clkFn(map, currentURL));
+
+      // binding popstate eventHandler again
+      $(window).one('popstate', popFn(map));
+
+      // regist url
+      currentURL = location.pathname;
+
+      // callback action.
+      options.callback[action].call(map._self, target);
     };
 
     // Cancel the current request if we're already pjaxing
@@ -95,6 +211,7 @@
     return pjax.xhr;
   };
 
+  // default settings on ajax
   pjax.defaults = {
     timeout: 650,
     push: true,
@@ -108,7 +225,7 @@
     type: 'GET',
     dataType: 'html',
     beforeSend: function(xhr) {
-      this.trigger('pjax:start', [xhr, pjax.options]);
+      $(this).trigger('pjax:start', [xhr, pjax.options]);
       xhr.setRequestHeader('X-PJAX', 'true');
     },
     error: function(xhr, textStatus, errorThrown) {
@@ -116,53 +233,21 @@
         window.location = pjax.options.url;
     },
     complete: function(xhr) {
-      this.trigger('pjax:end', [xhr, pjax.options]);
+      $(this).trigger('pjax:end', [xhr, pjax.options]);
     }
   };
-
-  // Used to detect initial (useless) popstate.
-  // If history.state exists, assume browser isn't going to fire initial popstate.
-  var popped = ('state' in window.history), initialURL = location.href;
-
-  // popstate handler takes care of the back and forward buttons
-  //
-  // You probably shouldn't use pjax on pages with other pushState
-  // stuff yet.
-  $(window).bind('popstate', function(event) {
-    // Ignore inital popstate that some browsers fire on page load
-    var initialPop = !popped && location.href == initialURL;
-    popped = true;
-    if(initialPop)
-      return;
-
-    var state = event.state;
-
-    if(state && state.pjax) {
-      var selector = state.pjax;
-      if($(selector + '').length)
-        $.pjax({
-          url: state.url || location.href,
-          fragment: state.fragment,
-          container: container,
-          push: false,
-          timeout: state.timeout
-        });
-      else
-        window.location = location.href;
-    }
-  });
 
   // Add the state property to jQuery's event object so we can use it in
   // $(window).bind('popstate')
   if($.inArray('state', $.event.props) < 0)
     $.event.props.push('state');
 
-    // Is pjax supported by this browser?
+  // Is pjax supported by this browser?
   $.support.pjax = window.history && window.history.pushState && window.history.replaceState
   // pushState isn't reliable on iOS yet.
   && !navigator.userAgent.match(/(iPod|iPhone|iPad|WebApps\/.+CFNetwork)/);
 
-  // Fall back to normalcy for older browsers.
+  // >> Fall back to normalcy for older browsers.
   if($.support.pjax !== true) {
     $.pjax = function(options) {
       window.location = $.isFunction(options.url) ? options.url(): options.url;
@@ -172,5 +257,6 @@
       return this;
     };
   }
+  // << Fall back end.
 
-})(jQuery);
+})(window.jQuery);
